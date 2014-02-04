c561fa0dff576720b5f8d8117e83e33c78e5302f
David Chang <dchang@adobe.com>
2014-02-04 15:15:10 -0500

    Now runs query on load and editor change events.

--- content/src/main/content/jcr_root/apps/acs-tools/components/query-editor/clientlibs/js/app.js
+++ content/src/main/content/jcr_root/apps/acs-tools/components/query-editor/clientlibs/js/app.js
@@ -1,27 +1,41 @@
 (function() {
 
-'use strict';
+    'use strict';
 
-var source = "type=nt:file\n" +
-             "nodename=*.jar\n" +
-             "orderby=@jcr:content/jcr:lastModified\n" +
-             "orderby.sort=desc";
+    var source = "type=nt:file\n" +
+        "nodename=*.jar\n" +
+        "orderby=@jcr:content/jcr:lastModified\n" +
+        "orderby.sort=desc";
 
-ace.require("ace/ext/language_tools");
+    var editor = ace.edit("editor");
+    var output = ace.edit("output");
 
-var editor = ace.edit("editor");
-editor.session.setMode("ace/mode/querybuilder");
-editor.setTheme("ace/theme/monokai");
-editor.setOptions({
-    enableBasicAutocompletion: true,
-    enableSnippets: true
-});
-editor.setValue(source, 0);
-//editor.on("change", );
+    var refresh = _.debounce(function() {
+        var toParams = function(source) {
+            return source.replace(/^\s+|\s+$/mg, "").replace(/^#.*$/mg, "").replace(/\n/g, "&");
+        }
 
-var output = ace.edit("output");
-output.session.setMode("ace/mode/json");
-output.setTheme("ace/theme/monokai");
-output.setReadOnly(true);
+        $.getJSON("/bin/querybuilder.json", toParams(editor.getValue()))
+            .done(function(json) {
+                output.session.setValue(JSON.stringify(json, null, 4));
+            });
+    }, 500);
+
+    ace.require("ace/ext/language_tools");
+
+    editor.session.setMode("ace/mode/querybuilder");
+    editor.setTheme("ace/theme/monokai");
+    editor.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true
+    });
+    editor.session.setValue(source);
+    editor.on("change", refresh);
+
+    output.session.setMode("ace/mode/json");
+    output.setTheme("ace/theme/monokai");
+    output.setReadOnly(true);
+
+    refresh();
 
 })();
